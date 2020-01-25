b9d8adfe8c4ce2aa311a8860d220705325e6a5e3
RaphaelSmith <thetutorialdoctor@gmail.com>
2020-01-25 11:10:21 -0500

    added hello world

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Learn-Machine-Learning
+
 My adventures in learning machine learning
+
+**Machine learning** - Subfield of AI. 
+
+- AlphaGo
+
+Machine Learning is where computers can learn from examples and experience
+
+
+**Classifier** - function. takes data as input and asigns label as output. Classify email as spam or not spam. Classify image as apple or orange
+
+**Supervised learning** - Create a classifier by finding patterns in examples
+
+**Steps:**
+
+1. Collect training data
+2. Train Classifier
+3. Make Predictions
+
+
+**Feature** - input to the classifier
+
+**Label** - output we want
+
+**Decisions tree**
+
+**Classifier** - box of rules. many types. input and output always same.
+
+If classifier is a box of rules. A Learning  Algorithm is the procedure that creates the rules by finding patterns in the training data. It may notice orange weighs more, so it creates a rule that the heavier the fruit, the more likely it is to be an apple.
+
+
+# Resources
+
+https://www.youtube.com/watch?v=cKxRvEZd3Mw
---
+++ hello_world.py
@@ -0,0 +1,38 @@
+from sklearn import tree
+
+# Compare apples and oranges. "0" is bumpy. "1" is smooth
+# 1. Collect training data
+# features = [
+#     [140,"smooth"],
+#     [130,"smooth"],
+#     [150,"bumpy"],
+#     [170,"bumpy"]
+# ]
+# Scikit uses real-valued features (can't use strings as features)
+features = [
+    [140,1],
+    [130,1],
+    [150,0],
+    [170,0]
+]
+
+#  "0" is apple. "1" is orange
+labels = ["apple", "apple", "orange", "orange"]
+
+# Create the classifier (empty box of rules)
+classifier = tree.DecisionTreeClassifier()
+
+# 2. Train Classifier. fit() is the training algorithm. 
+classifier = classifier.fit(features, labels)
+# fit() can be seen as a synomym for "find patterns in training data"
+
+# 3. Make Predictions
+print(classifier.predict([[170,0]])) #2 dimensional array needed for python3
+
+
+# THE MORE SCI-KIT WAY
+features = [[140,1],[130,1],[150,0],[170,0]]
+labels = [0, 0, 1, 1]
+classifier = tree.DecisionTreeClassifier()
+classifier = classifier.fit(features,labels)
+print(classifier.predict([[150,0]])) #2 dimensional array needed for python3
